7d767c1893b35e691d3ba38e4b5dbf4b8eba1caf
misha <miskripa@gmail.com>
2025-05-10 00:21:00 +0300

    fix

--- src/main/java/com/example/tech_spec/model/dto/SubDTO.java
+++ src/main/java/com/example/tech_spec/model/dto/SubDTO.java
@@ -7,6 +7,7 @@ import com.example.tech_spec.model.SubscriptionType;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SubDTO {
-  @NotBlank(message = "this column not be empty")
+  @NotNull(message = "this column not be empty")
       private SubscriptionType subscriptionType;
-      @NotBlank(message = "this column not be empty")
+      @NotNull(message = "this column not be empty")
     private LocalDate startDate;
-   @NotBlank(message = "this column not be empty")
+  @NotNull(message = "this column not be empty")
     private LocalDate endDate;
-   @NotBlank(message = "this column not be empty")
+   @NotNull(message = "this column not be empty")
     private BigDecimal monthlyPrice;
 }
